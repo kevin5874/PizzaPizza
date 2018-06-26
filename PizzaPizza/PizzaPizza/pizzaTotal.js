1b87a55fd9b025a8552251a55c4f1449178ae98d
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-06-25 22:38:26 -0500

    finishing up.

--- PizzaPizza/PizzaPizza/pizzaTotal.js
+++ PizzaPizza/PizzaPizza/pizzaTotal.js
@@ -1,10 +1,11 @@
 ﻿"use strict"
 var meats = 0.00;
-var size;
-var cheese;
-var crust;
-var sauce;
+var size = "Pizza Size:";
+var cheese ="Cheese: ";
+var crust = "Crust: ";
+var sauce = "Sauce: ";
 var veggies = 0.00;
+var toppings = "Toppings: "
 function totalCost() {
     if ($("#meatsModal input:checkbox:checked").length > 0) {
         meats = -1.00
@@ -13,13 +14,32 @@ function totalCost() {
         veggies = -1.00
     }
    $("input:checkbox:checked").each(function (i) {
-       meats += parseInt(($(this).val())); {
-       }
+       meats += parseInt(($(this).val())); 
+       toppings = toppings + ", " + ($(this).data('name'));
+       
             })
-        
-   
+    $("input:radio:checked").each(function (i) {
+        meats += parseInt(($(this).val()));
 
-    document.getElementById("total").innerHTML = veggies + meats;
+    })
+    $("#sizeModal input:radio:checked").each(function (i) {
+        size = size + " " + ($(this).data('name'));
+    })
+    $("#cheeseModal input:radio:checked").each(function (i) {
+        cheese = cheese + " " + ($(this).data('name'));
+    })
+    $("#sauceModal input:radio:checked").each(function (i) {
+        sauce = sauce + " " + ($(this).data('name'));
+    })
+    $("#crustModal input:radio:checked").each(function (i) {
+        crust = crust + " " + ($(this).data('name'));
+    })
+    
+    meats += veggies
+    let total = "$" + meats + ".00";
+    //Considered getting a sales tax api to calculate local sales tax, but couldnt find a free one.
+    document.getElementById("total").innerHTML = total;
+    alert(size+ "\n" +cheese+"\n"+sauce+"\n"+crust+ "\n" + toppings);
 
    
 
