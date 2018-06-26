2c98b897b41ff98eff374ca8a7e3ca45411b0988
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-06-25 21:43:44 -0500

    summing checkboxes for the total pizza cost

---
+++ PizzaPizza/PizzaPizza/pizzaTotal.js
@@ -0,0 +1,29 @@
+﻿"use strict"
+var meats = 0.00;
+var size;
+var cheese;
+var crust;
+var sauce;
+var veggies = 0.00;
+function totalCost() {
+    if ($("#meatsModal input:checkbox:checked").length > 0) {
+        meats = -1.00
+    }
+    if ($("#veggiesModal input:checkbox:checked").length > 0) {
+        veggies = -1.00
+    }
+   $("input:checkbox:checked").each(function (i) {
+       meats += parseInt(($(this).val())); {
+       }
+            })
+        
+   
+
+    document.getElementById("total").innerHTML = veggies + meats;
+
+   
+
+}
+
+
+
